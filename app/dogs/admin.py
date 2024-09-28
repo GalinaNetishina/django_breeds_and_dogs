865066e1f88de52257ea4fa257501b6378670ca0
Galina Netishina <netishinagalina@yandex.ru>
2024-09-28 13:10:50 +0300

    small correct

--- app/dogs/admin.py
+++ app/dogs/admin.py
@@ -6,5 +6,5 @@ from .models import Dog
 
 @admin.register(Dog)
 class DogAdmin(admin.ModelAdmin):
-    list_display = ["name", "gender", "age", "color", "breed__name"]
+    list_display = ["name", "gender", "age", "color", "breed"]
     ordering = ["name", "gender", "age"]
--- app/dogs/models.py
+++ app/dogs/models.py
@@ -12,8 +12,8 @@ class Dog(models.Model):
     breed = models.ForeignKey(Breed, on_delete=models.SET_DEFAULT, default="Unknown")
     gender = models.CharField(choices=Gender, max_length=6, default="male")
     color = models.CharField(max_length=32)
-    favorite_food = models.CharField(max_length=40, null=True)
-    favorite_toy = models.CharField(max_length=30, null=True)
+    favorite_food = models.CharField(max_length=40, blank=True)
+    favorite_toy = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
-        return f"{self.name}, {self.gender}, {self.age}"
+        return f"{self.name}, {self.gender}, {self.age}, {self.breed.name}"

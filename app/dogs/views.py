e8aadaf3fcd1ce67093abfb0858da2591d823a09
Galina Netishina <netishinagalina@yandex.ru>
2024-09-28 12:40:10 +0300

    get rid of excess

--- app/dogs/views.py
+++ app/dogs/views.py
@@ -7,22 +7,7 @@ class DogList(generics.ListCreateAPIView):
     queryset = Dog.objects.prefetch_related("breed").all()
     serializer_class = DogSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
 
 class DogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Dog.objects.all().select_related()
     serializer_class = DogSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
